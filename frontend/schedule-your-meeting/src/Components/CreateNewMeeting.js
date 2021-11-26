0d88be9bb145b7b9af93c682a918297f61e1d0e8
JanMoritzP <janmoritz@pocher.de>
2021-11-26 12:06:06 +0100

    Fixed some issues with value

--- frontend/schedule-your-meeting/src/Components/CreateNewMeeting.js
+++ frontend/schedule-your-meeting/src/Components/CreateNewMeeting.js
@@ -2,66 +2,6 @@ import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
 
 export default function CreateNewMeeting() {
-    const date = new Date();
-    
-    const [name, setName] = useState("");
-    const [uniqueCheck, setUniqueCheck] = useState("X")
-    const [startingDate, setStartingDate] = useState(date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate());
-    const [endingDate, setEndingDate] = useState();
-    const navigate = useNavigate();
-
-
-    const handleSubmit = async e => {
-        e.preventDefault()
-        if(name !== "") {
-            fetch("http://localhost:3080/createNewMeeting", {
-                method: "POST", 
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: name,
-                    password: e.target.password.value,
-                    startingDate: startingDate,
-                    endingDate: endingDate,
-                    participantAmount: e.target.participantAmount.value
-                })
-            })
-            .then(res => {
-                if(res.status === 200) {
-                    localStorage.setItem('password', e.target.password.value)
-                    navigate("/meeting/" + name)
-                }
-                else {
-                    console.log(res);
-                }
-            })
-        }
-    }
-
-    useEffect(() => {
-        const delayUniqueNameCheck = setTimeout(() => {
-            //Send the request here
-            if(name !== "") {
-                fetch("http://localhost:3080/checkUniqueMeeting", {
-                    method: "POST", 
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        name: name
-                    })
-                })
-                .then(res => {
-                    if(res.status === 200) setUniqueCheck("Yeah")
-                    else setUniqueCheck("X")
-                })
-            }
-            else setUniqueCheck("X")
-            
-        }, 500) //ms
-        return () => clearTimeout(delayUniqueNameCheck)
-    }, [name])
 
     const getMinDate = inputDate => {
         const [year, month, day] = inputDate.split('-');
@@ -118,6 +58,69 @@ export default function CreateNewMeeting() {
         else return string
     }
 
+    const date = new Date();
+    
+    const [name, setName] = useState("");
+    const [uniqueCheck, setUniqueCheck] = useState("X")
+    const [startingDate, setStartingDate] = useState(date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate());
+    const [endingDate, setEndingDate] = useState(getMinDate(startingDate));
+    const navigate = useNavigate();
+
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+        if(name !== "") {
+            fetch("http://localhost:3080/createNewMeeting", {
+                method: "POST", 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: name,
+                    password: e.target.password.value,
+                    startingDate: startingDate,
+                    endingDate: endingDate,
+                    participantAmount: e.target.participantAmount.value
+                })
+            })
+            .then(res => {
+                if(res.status === 200) {
+                    localStorage.setItem('password', e.target.password.value)
+                    navigate("/meeting/" + name)
+                }
+                else {
+                    console.log(res);
+                }
+            })
+        }
+    }
+
+    useEffect(() => {
+        const delayUniqueNameCheck = setTimeout(() => {
+            //Send the request here
+            if(name !== "") {
+                fetch("http://localhost:3080/checkUniqueMeeting", {
+                    method: "POST", 
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: name
+                    })
+                })
+                .then(res => {
+                    if(res.status === 200) setUniqueCheck("Yeah")
+                    else setUniqueCheck("X")
+                })
+            }
+            else setUniqueCheck("X")
+            
+        }, 500) //ms
+        return () => clearTimeout(delayUniqueNameCheck)
+    }, [name])
+
+    
+
     return(
         <div>
             <h2>Create a new meeting</h2>
@@ -129,9 +132,9 @@ export default function CreateNewMeeting() {
                 <label for="startingDate">Starting Date</label>
                 <input type="date" id="startingDate" name="startingDate"
                 min={date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate()} value={startingDate}
-                onChange={e => {setStartingDate(e.target.value)}}></input>
+                onChange={e => {setStartingDate(e.target.value);setEndingDate(getMinDate(e.target.value))}}></input>
                 <label for="endingDate">Ending Date</label>       
-                <input type="date" id="endingDate" name="endingDate" min={getMinDate(startingDate)} value={getMinDate(startingDate)} onChange={e => setEndingDate(e.target.value)}></input>     
+                <input type="date" id="endingDate" name="endingDate" min={getMinDate(startingDate)} value={endingDate} onChange={e => setEndingDate(e.target.value)}></input>     
                 <label for="participantAmount">Amount of participants</label>
                 <input type="number" id="participantAmount" name="participantAmount"></input>           
                 <input type="submit" value="Create"></input>
