e02a2e1636a98d0e15dcca7c3c49c7b47038d9bf
JanMoritzP <janmoritz@pocher.de>
2021-12-20 09:33:59 +0100

    Fixed user check line

--- backend/routes/addNewUser.js
+++ backend/routes/addNewUser.js
@@ -14,7 +14,7 @@ const Meeting = require('../Schema/Meeting')
 //mongoose.connect('mongodb://localhost:27017/scheduleMeeting', {useNewUrlParser:true, useUnifiedTopology:true})
 
 router.post('/addNewUser', (req, res) => {
-    if(req.body === "") return res.status(409).send({message: 'You have to enter a name for the new user'})
+    if(req.body.user === "") return res.status(409).send({message: 'You have to enter a name for the new user'})
     Meeting.findOne({name: req.body.meeting}, (err, meeting) => {
         if(err) res.status(500).send({message: 'Error accessing the database'})
         else if(!meeting) res.status(404).send({messsage: 'There is no meeting with the id ' + req.body.meeting})
