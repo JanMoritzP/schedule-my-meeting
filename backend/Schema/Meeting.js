7213c48958db59548fa73d486a847d4763381332
JanMoritzP <janmoritz@pocher.de>
2021-12-20 08:48:30 +0100

    Edited the schema to add in an admin for the meetings

--- backend/Schema/Meeting.js
+++ backend/Schema/Meeting.js
@@ -9,7 +9,10 @@ const MeetingSchema = mongoose.Schema({
     salt: String,
     name: {type: String, unique: true, required: true},
     startingDate: {type: String, required: true},
-    endingDate: {type: String, required: true}
+    endingDate: {type: String, required: true},
+    admin: {type: String, required: true},
+    adminHash: String,
+    adminSalt: String
 })
 
 MeetingSchema.methods.setPassword = function(password) {
@@ -20,6 +23,15 @@ MeetingSchema.methods.setPassword = function(password) {
 MeetingSchema.methods.validatePassword = function(password) {
     tmpHash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
     return this.hash == tmpHash
+}
+MeetingSchema.methods.setAdminPassword = function(password) {
+    this.adminSalt = crypto.randomBytes(32)
+    this.adminHash = crypto.pbkdf2Sync(password, this.adminSalt, 1000, 64, `sha512`).toString(`hex`); 
+}
+
+MeetingSchema.methods.validateAdminPassword = function(password) {
+    tmpAdminHash = crypto.pbkdf2Sync(password, this.adminSalt, 1000, 64, `sha512`).toString(`hex`);
+    return this.adminHash == tmpAdminHash
 }
 
 const Meeting = module.exports = mongoose.model('meeting', MeetingSchema, 'scheduleMeeting')
