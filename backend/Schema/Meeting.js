ba172a5508c71e5c7cb86eba51b5fa3b93991b01
JanMoritzP <janmoritz@pocher.de>
2022-01-03 08:58:40 +0100

    Removed admin stuff and added expiration

--- backend/Schema/Meeting.js
+++ backend/Schema/Meeting.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const crypto = require('crypto')
 
 const MeetingSchema = mongoose.Schema({
+    sessionActivity: {type: Date, expires: 1209600, default: Date.now}, //Two weeks expiration
     participants: [String],
     participantAmount: {type: Number, required: true},
     timeData: [String],
@@ -10,10 +11,7 @@ const MeetingSchema = mongoose.Schema({
     salt: String,
     name: {type: String, unique: true, required: true},
     startingDate: {type: String, required: true},
-    endingDate: {type: String, required: true},
-    admin: {type: String},
-    adminHash: String,
-    adminSalt: String
+    endingDate: {type: String, required: true}
 })
 
 MeetingSchema.methods.setPassword = function(password) {
@@ -24,15 +22,6 @@ MeetingSchema.methods.setPassword = function(password) {
 MeetingSchema.methods.validatePassword = function(password) {
     tmpHash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, `sha512`).toString(`hex`);
     return this.hash == tmpHash
-}
-MeetingSchema.methods.setAdminPassword = function(password) {
-    this.adminSalt = crypto.randomBytes(32)
-    this.adminHash = crypto.pbkdf2Sync(password, this.adminSalt, 1000, 64, `sha512`).toString(`hex`); 
-}
-
-MeetingSchema.methods.validateAdminPassword = function(password) {
-    tmpAdminHash = crypto.pbkdf2Sync(password, this.adminSalt, 1000, 64, `sha512`).toString(`hex`);
-    return this.adminHash == tmpAdminHash
 }
 
 const Meeting = module.exports = mongoose.model('meeting', MeetingSchema, 'scheduleMeeting')
