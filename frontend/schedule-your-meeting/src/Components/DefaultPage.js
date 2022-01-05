f337a1ad11ecbfbe4c2d371f53eb528de60cd51d
JanMoritzP <janmoritz@pocher.de>
2022-01-05 16:01:35 +0100

    Started working on css

--- frontend/schedule-your-meeting/src/Components/DefaultPage.js
+++ frontend/schedule-your-meeting/src/Components/DefaultPage.js
@@ -5,9 +5,9 @@ export default function DefaultPage() {
 
     return(
         <div>
-            <h2>Welcome to schedule your meeting</h2>
+            <h2 id="infoHeader">Welcome to schedule your meeting</h2>
             <div>
-                <p>
+                <p id="infoParagraph">
                     You can use this website to create a meeting and then schedule it with your colleagues or friends.
                     You can either use the navbar on the left, or click <Link to={'/createNewMeeting'}>here</Link> to create a new meeting.
                     Every meeting has to have a unique name. You can set it yourself, and reuse it, if you want to schedule another meeting.
