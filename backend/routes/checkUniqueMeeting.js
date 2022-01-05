a001ebd4edeb14223ed4faa66ceef023f19a1684
JanMoritzP <janmoritz@pocher.de>
2022-01-05 15:32:41 +0100

    Fixed routing issues

--- backend/routes/checkUniqueMeeting.js
+++ backend/routes/checkUniqueMeeting.js
@@ -13,7 +13,7 @@ router.use((req, res, next) => {
 const Meeting = require('./../Schema/Meeting')
 //mongoose.connect('mongodb://localhost:27017/scheduleMeeting', {useNewUrlParser:true, useUnifiedTopology:true})
 
-router.post('data//checkUniqueMeeting', (req, res) => {
+router.post('/data/checkUniqueMeeting', (req, res) => {
     if(req.body === "") return res.status(409).send({message: 'You have to enter a name for the meeting'})
     Meeting.findOne({name: req.body.name}, (err, meeting) => {
         if(err) res.status(500).send({message: 'Error accessing the database'})
