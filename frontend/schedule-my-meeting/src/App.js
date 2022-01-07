abc138db7229366a9df544ca45c615af123ccbe0
JanMoritzP <janmoritz@pocher.de>
2022-01-07 08:31:15 +0100

    Removed navbar

--- frontend/schedule-my-meeting/src/App.js
+++ frontend/schedule-my-meeting/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './Components/css/App.css';
 import DefaultPage from './Components/DefaultPage';
-import Navbar from './Components/Navbar';
 import CreateNewMeeting from './Components/CreateNewMeeting';
 import Meeting from './Components/Meeting'
 
@@ -13,7 +12,6 @@ function App() {
   return (
     <div>
       <h1>Schedule your meeting!</h1>
-          <Navbar/>
           
           <Routes>
             <Route path="/createNewMeeting/" element={<CreateNewMeeting/>}/>
